5f2b9c7caeedd0c0c348acc614f20622ea2281b7
Seokxkyu <kyuseok000@gmail.com>
2024-07-30 16:04:01 +0900

    echo func and version up

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,8 +1,12 @@
 # test_call.py
 
-from mov.api.call import gen_url, req, get_key, req2list, list2df, save2df
+from mov.api.call import gen_url, req, get_key, req2list, list2df, save2df, echo
 # from mov.api.call import *
 import pandas as pd
+
+def test_echo():
+    r = echo("hello")
+    assert r == "hello"
 
 def test_save2df():
     df = save2df()
